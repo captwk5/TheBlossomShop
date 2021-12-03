66c0eda948a2c8c78ebafc3c228439cc9d03990b
wkh <captwk5@gmail.com>
2021-12-04 01:51:44 +0900

    in the middle of thumbnail list

---
+++ app/src/main/java/com/wksy/theblossomshop/ThumbNailAdapter.kt
@@ -0,0 +1,24 @@
+package com.wksy.theblossomshop
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ThumbNailAdapter : RecyclerView.Adapter<ThumbNailAdapter.ViewHolder>() {
+
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
